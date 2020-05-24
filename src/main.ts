2b94c77363ecf87ab88f7122a27779c80e9e56b8
kazuya kawaguchi <kawakazu80@gmail.com>
2020-05-24 22:42:21 +0900

    tweak indent

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import en from './locales/en.json'
 import ja from './locales/ja.json'
 
 const i18n = createI18n({
-    locale: 'en',
-    messages: {
-        en,
-        ja
-    }
+  locale: 'en',
+  messages: {
+    en,
+    ja
+  }
 })
 
 createApp(App).use(i18n).mount('#app')
