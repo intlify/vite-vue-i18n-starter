e83e6931d74d39cec87520473fa6a467a303792f
kazuya kawaguchi <kawakazu80@gmail.com>
2021-01-01 05:45:07 +0900

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { pluginI18n } from '@intlify/vite-plugin-vue-i18n'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
+  alias: {
+    'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
+  },
   plugins: [
     pluginI18n({
       include: path.resolve(__dirname, './src/locales/**')
