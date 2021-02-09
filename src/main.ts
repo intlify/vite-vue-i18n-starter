aea5c78472987dc238ede5bae441f63995c4a48a
kazuya kawaguchi <kawakazu80@gmail.com>
2021-02-09 14:43:10 +0900

    updates

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,14 @@ import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import './index.css'
-import en from './locales/en.json'
-import ja from './locales/ja.json'
+// About the below import
+// See https://github.com/intlify/vite-plugin-vue-i18n#static-bundle-importing
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
 
 const i18n = createI18n({
   legacy: false,
   locale: 'en',
-  messages: {
-    en,
-    ja
-  }
+  messages
 })
 
 createApp(App).use(i18n).mount('#app')
