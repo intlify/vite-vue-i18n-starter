4e2e644f5a5497e6b5d0a9af52a0b9b762a23958
kazuya kawaguchi <kawakazu80@gmail.com>
2021-02-23 18:51:28 +0900

    update

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -1,6 +1,22 @@
 <template>
   <h1>{{ t('hello') }}</h1>
+
   <button @click="count++">{{ t('count', { count }) }}</button>
+
+  <p>
+    <a href="https://vue-i18n.intlify.dev/" target="_blank">{{ t('docs.vue-i18n') }}</a> |
+    <a href="https://github.com/intlify/vite-plugin-vue-i18n#globe_with_meridians-intlifyvite-plugin-vue-i18n" target="_blank">{{ t('docs.vite-plugin') }}</a>
+  </p>
+
+  <p>
+    {{ t('recommended') }}:
+    <a href="https://github.com/lokalise/i18n-ally/" target="_blank">i18n ally (Visutal Studio Code extension)</a>
+    +
+    <a
+      href="https://eslint-plugin-vue-i18n.intlify.dev/"
+      target="_blank"
+    >eslint-plugin-vue-i18n</a>
+  </p>
 </template>
 
 <script lang="ts">
@@ -27,11 +43,21 @@ export default defineComponent({
 {
   "en": {
     "hello": "Hello Vue I18n 9.0 🌏!",
-    "count": "count is: {count}"
+    "count": "count is: {count}",
+    "docs": {
+      "vue-i18n": "Vue I18n v9 Documentation",
+      "vite-plugin": "Vite Plugin Vue I18n Documentation"
+    },
+    "recommended": "Recommended setup"
   },
   "ja": {
     "hello": "こんにちは Vue I18n 9.0 🌏！",
-    "count": "カウントは{count}です"
+    "count": "カウントは{count}です",
+    "docs": {
+      "vue-i18n": "Vue I18n v9 ドキュメント",
+      "vite-plugin": "Vite プラグイン Vue I18n ドキュメント"
+    },
+    "recommended": "推奨セットアップ"
   }
 }
 </i18n>
