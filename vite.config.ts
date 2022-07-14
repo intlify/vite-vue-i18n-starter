65c803b2cf3397747bbc34c27031924a7dda4697
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-14 18:57:00 +0900

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from 'path'
+import { resolve, dirname } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import { vueI18n } from '@intlify/vite-plugin-vue-i18n'
+import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueI18n({
-      include: path.resolve(__dirname, './src/locales/**'),
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
     })
   ]
 })
