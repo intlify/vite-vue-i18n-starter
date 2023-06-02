1dcaa0e2d1e0cb0deaa50759c9c101dc03ce4588
kazuya kawaguchi <kawakazu80@gmail.com>
2023-06-02 18:33:41 +0900

    updates

---
+++ src/components/Parent.vue
@@ -0,0 +1,19 @@
+<template>
+  <Test>
+    component: <i18n-t keypath="doesNotWork" />
+    <br />
+    t: {{ t('doesNotWork') }}
+  </Test>
+</template>
+
+<script setup lang="ts">
+import Test from './Test.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n() // TODO: https://github.com/intlify/vue-i18n-next/issues/1274
+</script>
+
+<i18n lang="yaml">
+en:
+  doesNotWork: 'works'
+</i18n>
---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot />
+  <p>{{ x.t('any') }}</p>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+// comment/remove the line below to make translation work again!
+const x = useI18n();
+// alternatively, remove the <i18n> block below for the same effect)
+</script>
+
+<i18n lang="yaml">
+en:
+  any: thing
+</i18n>
